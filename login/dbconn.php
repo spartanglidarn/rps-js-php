a419f0aa4061130a4cf0977122d367d8f7a9293e
spartanglidarn <erikbostrom1@gmail.com>
2016-11-13 22:14:03 +0100

    new db

--- login/dbconn.php
+++ login/dbconn.php
@@ -1,9 +1,19 @@
 <?php
-//databas information
+$url = getenv('JAWSDB_URL');
+$dbparts = parse_url($url);
+
+$servername = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$dbname = ltrim($dbparts['path'],'/');
+
+
+/*
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "login";
+*/
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
